eee580df5c296daa1e6a2912231d4836860a632e
TJ Muehleman <tj.muehleman@gmail.com>
2013-12-09 09:32:37 -0500

    starting our path to being thousandaires

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Quilt::Application.config.secret_key_base = 'cc3fb9f29270d3ca730ef5fcc9d47745ce9f0ce3e93c5526998324538b147ad7e443cf650122c34d1b37a65ec3894e08c0adf1c875e2785f3bb40b3319eb6120'
