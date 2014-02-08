dd44d980f372e4c180761d69d362ef4e3ff4252a
TJ Muehleman <tj.muehleman@gmail.com>
2014-02-07 23:20:37 -0500

    added form to manual

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,8 @@ Quilt::Application.routes.draw do
   get "/components/upload"
   get "/components/github"
 
+  post "/components/manual", to: "components#save_manual"
+
   resources :styleguides
   resources :components
 
