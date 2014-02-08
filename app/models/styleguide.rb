50d57611fa53d58673e47d10a6b5879be0f603cb
TJ Muehleman <tj.muehleman@gmail.com>
2014-02-07 19:26:44 -0500

    added category to styleguide

--- app/models/styleguide.rb
+++ app/models/styleguide.rb
@@ -4,7 +4,10 @@ class Styleguide
   field :user_id, type: String
   field :is_public, type: Integer
   field :css_path, type: String
-  field :tags, type: String
+  #field :tags, type: String
+  field :description, type: String
+  field :url, type: String
+  field :categories, type: String
 
   belongs_to :user
 end
