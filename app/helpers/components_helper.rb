18d0196b836b592e4609f2e73d8abf398f76e26e
Jonathan Nesbitt <jon.nesbitt@gmail.com>
2014-02-14 17:36:39 -0500

    Fixed the 'edit component' page.

--- app/helpers/components_helper.rb
+++ app/helpers/components_helper.rb
@@ -1,2 +1,10 @@
 module ComponentsHelper
+  def css_links(styleguide)
+    if styleguide.css_paths
+      styleguide.css_paths.split(",").each do |css_path|
+        # TODO: Build the link tags.
+      end
+    end
+    css_links
+  end
 end
