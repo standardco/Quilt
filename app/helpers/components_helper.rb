95a2a7e3d26807d6e67d36be3f03d6ba6772baa4
Jonathan Nesbitt <jon.nesbitt@gmail.com>
2014-02-15 11:51:06 -0500

    Fixed a bug in the components helper.

--- app/helpers/components_helper.rb
+++ app/helpers/components_helper.rb
@@ -1,10 +1,5 @@
 module ComponentsHelper
   def css_links(styleguide)
-    if styleguide.css_paths
-      styleguide.css_paths.split(",").each do |css_path|
-        # TODO: Build the link tags.
-      end
-    end
-    css_links
+    # TODO: Build the link tags.
   end
 end
