3d847ac2e4cefda811cdb8f0c16a1db655f45a6d
Jonathan Nesbitt <jon.nesbitt@gmail.com>
2014-03-09 22:12:00 -0400

    Removed unused code.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,18 +4,11 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   before_filter :configure_devise_params, if: :devise_controller?
-  before_filter :component_tags
 
   def configure_devise_params
     devise_parameter_sanitizer.for(:sign_up) do |u|
       u.permit(:username, :email, :password, :password_confirmation)
     end
-  end
-
-  protected
-
-  def component_tags
-  	@components = Component.all
   end
 
 end
