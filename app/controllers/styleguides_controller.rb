7c60827d2e3022dea34aff31521f72da97ce0349
Jonathan Nesbitt <jon.nesbitt@gmail.com>
2014-03-09 23:01:15 -0400

    Adde authentication to StyleguidesController.

--- app/controllers/styleguides_controller.rb
+++ app/controllers/styleguides_controller.rb
@@ -1,6 +1,6 @@
 class StyleguidesController < ApplicationController
   before_action :set_styleguide, only: [:show, :edit, :update, :destroy]
-
+  before_filter :authenticate_user!
 
   # GET /:user (/tjmule)
   # GET /styleguides
